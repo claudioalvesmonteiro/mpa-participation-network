a250d644d6409336d8d6a3b85402020eacdecd67
Cláudio Monteiro <clam@cin.ufpe.br>
2020-05-26 17:10:52 -0300

    ata_download_code_write

--- codes/rqda.R
+++ codes/rqda.R
@@ -1,5 +1,21 @@
+#=======================================
+# paper marine policy
+#
+#
+#========================================
 
 
-
+# load package
 library(RQDA)
-RQDA()
+
+# visualizar a contagem de cada codigo
+sumario_cod <- summaryCodings()
+
+# contagem de cada codigo
+cont_cod_data <- data.frame(sumario_cod$NumOfCoding)
+
+# visualizar infos sobre cada codificacao
+coding_table <- getCodingTable()
+
+# export 
+write.csv('data/',coding_table, row.names = F)
