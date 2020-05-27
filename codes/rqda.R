d398ce503b51f3ebe87c73998a18842cf48f45e7
Cláudio Monteiro <clam@cin.ufpe.br>
2020-05-27 09:10:54 -0300

    exporting coding table

--- codes/rqda.R
+++ codes/rqda.R
@@ -5,8 +5,12 @@
 #========================================
 
 
-# load package
+# load packages
+library(readxl)
 library(RQDA)
+library(stringr)
+library(stringi)
+library(dplyr)
 
 # visualizar a contagem de cada codigo
 sumario_cod <- summaryCodings()
@@ -17,5 +21,17 @@ cont_cod_data <- data.frame(sumario_cod$NumOfCoding)
 # visualizar infos sobre cada codificacao
 coding_table <- getCodingTable()
 
+# importar base de conselheiros
+insti <- read_excel("data/preprocessed/APA_instituicoes.xlsx")
+
+# preprocess names to merge
+insti$codename <- paste0(insti$nome_consel,'_',insti$entidade_sigla)
+insti$codename <- str_replace_all(insti$codename, ' ', '_')%>%
+                          stri_trans_general("latin-ascii")%>%
+  
+coding_table
+
+# combinar com base de codificacoes
+
 # export 
-write.csv('data/',coding_table, row.names = F)
+write.csv(coding_table, 'data/preprocessed/coding_table.csv', row.names = F)
