6a1d95ce682dd630b132b663002f95c831d10ae6
Cláudio Monteiro <clam@cin.ufpe.br>
2020-05-28 07:54:41 -0300

    'update_codes'

--- codes/rqda.R
+++ codes/rqda.R
@@ -28,22 +28,27 @@ coding_table <- getCodingTable()
 # importar base de conselheiros
 insti <- read_excel("data/preprocessed/APA_instituicoes.xlsx")
 
-# preprocess names to merge
-insti$codename <- paste0(insti$nome_consel,'_',insti$entidade_sigla)
-insti$codename <- str_replace_all(insti$codename, ' ', '_')%>%
-                          stri_trans_general("latin-ascii")%>%
-                          tolower()
-  
-coding_table$codename <- stri_trans_general(coding_table$codename, "latin-ascii")%>%
-                       tolower()
+  # preprocess names to merge
 
+# function to standardize string
+cleanString <- function(string){
+  string_clean <- str_replace_all(string, ' ', '_')%>%
+    str_replace_all( '-', '')%>%
+    str_replace_all( '/', '_')%>%
+    stri_trans_general("latin-ascii")%>%
+    tolower()
+}
+
+insti$codename <- paste0(insti$nome_consel,'_',insti$entidade_sigla)
+insti <- data.frame(sapply(insti, function(x) cleanString(x)))
+coding_table$codename <- cleanString(coding_table$codename)
 
 # merge
-coding_tablex <- merge(coding_table, insti, by='codename')
+counselors <- merge(coding_table, insti, by='codename')
 
 # export 
-write.csv(coding_tablex, 'data/preprocessed/council_table.csv', row.names = F)
+write.csv(counselors, 'data/preprocessed/council_table.csv', row.names = F)
 
 # select conflict and cooperation and save
-coding_tabley = coding_table[coding_table$codename == 't_cat_conflito' | coding_table$codename == 't_cat_cooperacao',]
-write.csv(coding_tabley, 'data/preprocessed/conflict_table.csv', row.names = F)
+conflict_coop = coding_table[coding_table$codename == 't_cat_conflito' | coding_table$codename == 't_cat_cooperacao',]
+write.csv(conflict_coop, 'data/preprocessed/conflict_table.csv', row.names = F)
