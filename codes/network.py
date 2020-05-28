3e11693eb1e4a67fd3516a1c385d4004e2687620
Cláudio Monteiro <clam@cin.ufpe.br>
2020-05-28 07:41:20 -0300

    'update'

--- codes/network.py
+++ codes/network.py
@@ -1,7 +1,19 @@
+'''
+paper marine policy
+claudio monteiro & beatriz mesquita
+
+@claudioalvesmonteiro 2020
+'''
+
 # https://python-graph-gallery.com/325-map-colour-to-the-edges-of-a-network/
 
 
+# import packages
+import pandas as pd
 
+# import data
+councelors = pd.read_csv('data/preprocessed/coding_table.csv', sep=',')
+conflict_coop = pd.read_csv('data/preprocessed/conflict_table.csv')
 
 
 
--- codes/rqda.R
+++ codes/rqda.R
@@ -1,7 +1,8 @@
 #=======================================
 # paper marine policy
+# claudio monteiro & beatriz mesquita
 #
-#
+# @claudioalvesmonteiro 2020
 #========================================
 
 
@@ -11,6 +12,9 @@ library(RQDA)
 library(stringr)
 library(stringi)
 library(dplyr)
+
+# 
+RQDA()
 
 # visualizar a contagem de cada codigo
 sumario_cod <- summaryCodings()
@@ -28,10 +32,18 @@ insti <- read_excel("data/preprocessed/APA_instituicoes.xlsx")
 insti$codename <- paste0(insti$nome_consel,'_',insti$entidade_sigla)
 insti$codename <- str_replace_all(insti$codename, ' ', '_')%>%
                           stri_trans_general("latin-ascii")%>%
+                          tolower()
   
-coding_table
+coding_table$codename <- stri_trans_general(coding_table$codename, "latin-ascii")%>%
+                       tolower()
 
-# combinar com base de codificacoes
+
+# merge
+coding_tablex <- merge(coding_table, insti, by='codename')
 
 # export 
-write.csv(coding_table, 'data/preprocessed/coding_table.csv', row.names = F)
+write.csv(coding_tablex, 'data/preprocessed/council_table.csv', row.names = F)
+
+# select conflict and cooperation and save
+coding_tabley = coding_table[coding_table$codename == 't_cat_conflito' | coding_table$codename == 't_cat_cooperacao',]
+write.csv(coding_tabley, 'data/preprocessed/conflict_table.csv', row.names = F)
