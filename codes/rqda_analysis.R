7068d638e4616a238ad8ed30c2150a3a8af1b83f
Cláudio Monteiro <clam@cin.ufpe.br>
2020-09-09 15:42:23 -0300

    up

--- codes/rqda_analysis.R
+++ codes/rqda_analysis.R
@@ -12,23 +12,14 @@ library(RQDA)
 library(stringr)
 library(stringi)
 library(dplyr)
+library(readr)
 
-# 
+
 RQDA()
 
-# visualizar a contagem de cada codigo
-sumario_cod <- summaryCodings()
-
-# contagem de cada codigo
-cont_cod_data <- data.frame(sumario_cod$NumOfCoding)
-
-# visualizar infos sobre cada codificacao
-coding_table <- getCodingTable()
 
 # importar base de conselheiros
-insti <- read_excel("data/preprocessed/APA_instituicoes.xlsx")
-
-  # preprocess names to merge
+insti <- read_csv("data/preprocessed/consel_instituicoes.csv")
 
 # function to standardize string
 cleanString <- function(string){
@@ -39,16 +30,35 @@ cleanString <- function(string){
     tolower()
 }
 
-insti$codename <- paste0(insti$nome_consel,'_',insti$entidade_sigla)
-insti <- data.frame(sapply(insti, function(x) cleanString(x)))
+insti$codename <- cleanString(paste0(insti$nome_consel,'_',insti$entidade_sigla))
+cont_cod_data$codename <- cleanString(cont_cod_data$Var1)
+
+###########
+
+
+# visualizar a contagem de cada codigo
+sumario_cod <- summaryCodings()
+cont_cod_data <- data.frame(sumario_cod$NumOfCoding)
+
+# merge to verify
+cont_insti = merge(cont_cod_data, insti, by='codename', all.x = T)
+write.csv(cont_insti[,c(1,3,4)], 'cont_output_rqda.csv', row.names = F)
+
+
+###########
+
+# visualizar infos sobre cada codificacao
+coding_table <- getCodingTable()
+
+# clean string
 coding_table$codename <- cleanString(coding_table$codename)
 
 # merge
-counselors <- merge(coding_table, insti, by='codename')
+coding_table_final <- merge(coding_table, insti, by='codename')
 
 # export 
-write.csv(counselors, 'data/preprocessed/council_table.csv', row.names = F)
+write.csv(coding_table_final, 'data/preprocessed/council_table.csv', row.names = F)
 
 # select conflict and cooperation and save
 conflict_coop = coding_table[coding_table$codename == 't_cat_conflito' | coding_table$codename == 't_cat_cooperacao',]
-write.csv(conflict_coop, 'data/preprocessed/conflict_table.csv', row.names = F)
+write.csv(conflict_coop, 'data/preprocessed/conflict_coop_table.csv', row.names = F)
